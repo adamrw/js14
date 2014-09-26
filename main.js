231e2edb92cdf57cedf2df51f49a84ff5cdf4a5f
Adam Weiner <adamwineguy@gmail.com>
2014-09-26 11:56:12 -0600

    cleaned up excess code

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).on('ready', function() {
   
 var outerShell=$('<div class="outerShell"></div>');
 var innerShell=$('<div class="innerShell"></div>');
-// var leftAm=$('<div class="leftAm">AM</div>');
 var leftPm=$('<div class="leftPm">PM</div?>');
 var leftAuto=$('<div class="leftAuto">Auto</div>');
 var clockScreen=$('<div class="clockScreen"></div>');
@@ -11,31 +10,13 @@ var clockText=$('<div class="clockText"><p>12 : 17</p></div>');
 var amFrequencies=$('<div class="amFrequencies"><p>AM    53 60 70 90 110 140 170    KHz</p></div>');
 var fmFrequencies=$('<div class="fmFrequencies"><p>FM     88 92 96 102 106 108      MHz</p></div>');
 
-// function startTime(){
-// 	var today=new Date();
-// 	var h=today.getHours();
-// 	var m=today.getMinutes();
-// 	h=checkTime(h);
-// 	m=checkTime(m);
-// 	var theTime=h + ' : ' + m;
-
-// $('.clockText').html(theTime);
-// }
-
-
-
 $('.container').append(outerShell); 
 outerShell.append(innerShell);
-// innerShell.append(leftAm);
 innerShell.append(leftPm);
 innerShell.append(leftAuto);
 innerShell.append(clockScreen);
 clockScreen.append(ampmIndicator);
-// clockText.append(startTime);
 clockScreen.append(clockText);
 innerShell.append(amFrequencies);
 innerShell.append(fmFrequencies);
-
-
-
 });
